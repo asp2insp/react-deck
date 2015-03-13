e5cfd470ce7e6423afbd1ac6c8a405e13bc5c18f
Josiah Gaskin <josiahgaskin@gmail.com>
2015-03-13 00:00:09 -0700

    Fixing webpack warnings

--- src/components/testSlides/testSlidesStore.js
+++ src/components/testSlides/testSlidesStore.js
@@ -1,16 +1,17 @@
-var Store = require('nuclear-js').Store;
+var Store = require("nuclear-js").Store;
 var jadeContent = require("./testSlides.jade");
 var Immutable = require("immutable");
+var $ = require("jquery");
 
 var testSlidesStore = Store({
   getInitialState: function() {
-    var rootSelector = "article";
+    //var rootSelector = "article";
     var slideSelector = "section";
     $("body").append($("<div id=\"rd-hidden-content\">"));
-    $(this.props.domTemplate()).appendTo($("#rd-hidden-content"));
+    $(jadeContent()).appendTo($("#rd-hidden-content"));
     return Immutable.Map({
       slides: $("#rd-hidden-content")
-                .find(this.props.slideSelector)
+                .find(slideSelector)
                 .map(function (index, slide) {
                   return slide.innerHTML;
                 })
@@ -20,9 +21,9 @@ var testSlidesStore = Store({
   initialize: function() {
     // this will get called via `reactor.dispatch('createSlides')`
     // where the payload is a list of slides
-    this.on('createSlides', function(slides) {
+    this.on("createSlides", function(slides) {
       return slides;
-    })
+    });
   }
 });
 
